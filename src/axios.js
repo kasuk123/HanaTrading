b68b8486e4bdd2fbfddb09f6576185b2f4c83717
강현우 <hyeonu123@nate.com>
2025-06-15 13:57:25 +0900

    frontend the end

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 // ✅ 운영용 EC2 백엔드 주소 (8080포트)
-axios.defaults.baseURL = 'http://18.116.149.10:8080'; // ← EC2 퍼블릭 IP 꼭 사용
+axios.defaults.baseURL = 'http://hana-tradingcar.com'; // ← EC2 퍼블릭 IP 꼭 사용
 
 axios.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
