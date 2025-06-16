eedac16ed10aa2a5da57b885d8a4ad229bd785c9
강현우 <hyeonu123@nate.com>
2025-06-16 12:52:58 +0900

    url 수정

--- src/axios.js
+++ src/axios.js
@@ -2,10 +2,10 @@
 
 import axios from 'axios';
 
-// ✅ 운영 서버 EC2의 Tailscale 고정 IP 주소 사용
-axios.defaults.baseURL = 'http://100.79.123.33:8080'; // 실제 백엔드 서버 주소
+//  운영 서버 EC2의 Tailscale 고정 IP 주소 사용
+axios.defaults.baseURL = 'https://api.hana-tradingcar.com';
 
-// ✅ JWT 토큰이 있을 경우 자동으로 Authorization 헤더에 추가
+// ✅JWT 토큰이 있을 경우 자동으로 Authorization 헤더에 추가
 axios.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
